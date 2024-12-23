a733b6fd91cf2ea14ef2cf1a558f2f376177419c
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-12-23 21:01:32 +0200

    nex.js rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
+      <p>{new Date().toTimeString()}</p>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
